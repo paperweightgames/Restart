cc0178729027465cf53f886162002a1ae1695a90
Justin <jgrigas@elam.co.uk>
2019-03-28 11:54:34 +0000

    Custom shader, Balance

---
+++ Assets/Scripts/Player/PlayerBalance.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace Player {
+	public class PlayerBalance : MonoBehaviour
+	{
+		[SerializeField] private int balance;
+
+		public void ChangeMoney(int amount)
+		{
+			balance += amount;
+		}
+
+		public int GetBalance()
+		{
+			return balance;
+		}
+	}
+}
---
+++ Assets/Scripts/UI/BalanceDisplay.cs
@@ -0,0 +1,22 @@
+﻿using Player;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI {
+	[RequireComponent(typeof(Text))]
+	public class BalanceDisplay : MonoBehaviour
+	{
+		[SerializeField] private PlayerBalance playerBalance;
+		private Text _text;
+
+		private void Awake()
+		{
+			_text = GetComponent<Text>();
+		}
+
+		private void Update()
+		{
+			_text.text = $"Balance: {playerBalance.GetBalance():c2}";
+		}
+	}
+}
