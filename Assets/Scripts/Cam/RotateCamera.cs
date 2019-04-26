7bf59f8571c0074ba9ea68791f3d141338b3d0a2
Justin <jgrigas@elam.co.uk>
2019-04-26 15:54:44 +0100

    Added the new building

--- Assets/Scripts/Cam/RotateCamera.cs
+++ Assets/Scripts/Cam/RotateCamera.cs
@@ -6,14 +6,19 @@ namespace Cam
 	public class RotateCamera : MonoBehaviour
 	{
 		[SerializeField] private MasterInput _inputSystem;
-		[SerializeField] private Vector2 rotateSensitivity = new Vector2(2, 2);
-		[SerializeField, Tooltip("X is min rotation, Y is max rotation.")] private Vector2 cameraBounds;
+		[SerializeField] private Vector2 _rotateSensitivity = new Vector2(2, 2);
+		[SerializeField, Tooltip("X is min rotation, Y is max rotation.")] private Vector2 _cameraBounds;
 		private Vector3 _angle;
 
 		private void OnEnable()
 		{
 			_inputSystem.Player.Look.Enable();
 			_inputSystem.Player.Look.performed += context => Rotate(context.ReadValue<Vector2>());
+		}
+
+		private void OnDisable()
+		{
+			_inputSystem.Player.Look.Disable();
 		}
 
 		private void Start()
@@ -25,14 +30,13 @@ namespace Cam
 		private void Rotate(Vector2 input)
 		{
 			input *= Time.deltaTime;
-			print(input);
 			// Apply the sensitivity to the input.
-			var inputVector = new Vector3(-input.x * rotateSensitivity.x, input.y * rotateSensitivity.y, 0);
+			var inputVector = new Vector3(input.y * _rotateSensitivity.y, input.x * _rotateSensitivity.x, 0);
 			// Add the input to the current angle.
 			_angle += inputVector;
 			
 			// Keep the angle clamped between the bounds.
-			_angle.x = Mathf.Clamp(_angle.x, cameraBounds.x, cameraBounds.y);
+			_angle.x = Mathf.Clamp(_angle.x, _cameraBounds.x, _cameraBounds.y);
 			
 			// Apply the target angle to the GameObject.
 			transform.eulerAngles = _angle;
--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -5,42 +5,45 @@ namespace Player
 	[RequireComponent(typeof(Rigidbody))]
 	public class PlayerMovement : MonoBehaviour
 	{
-		[SerializeField] private AlignTransform.AlignTransform alignTransform;
-		[SerializeField] private float movementSpeed;
+		[SerializeField] private MasterInput _masterInput;
+		[SerializeField] private AlignTransform.AlignTransform _alignTransform;
+		[SerializeField] private float _movementSpeed;
+		[SerializeField] private Vector2 _moveForce;
 		private Rigidbody _rb;
 
 		private void Awake()
 		{
+			_masterInput.Player.Move.Enable();
+			_masterInput.Player.Move.performed += context => SetMoveForce(context.ReadValue<Vector2>());
 			// Get the reference to the rigid body component that's attached to this GameObject.
 			_rb = GetComponent<Rigidbody>();
 		}
 
 		private void FixedUpdate()
 		{
-			// Get input.
-			var h = Input.GetAxis("Horizontal");
-			var v = Input.GetAxis("Vertical");
-			
 			// Stop aligning the player with the camera.
-			alignTransform.enabled = false;
-			
-			// Move the player if any input is detected.
-			if (Mathf.Abs(h) + Mathf.Abs(v) > 0)
-			{
-				MovePlayer(new Vector2(h, v));
-			}
+			_alignTransform.enabled = false;
+
+			MovePlayer(_moveForce);
 		}
 
+		private void SetMoveForce(Vector2 newForce)
+		{
+			_moveForce = newForce;
+			print(_moveForce);
+		}
+		
 		private void MovePlayer(Vector2 input)
 		{
+			input *= Time.deltaTime;
 			// If the player is moving, align them with the camera.
-			alignTransform.enabled = true;
+			_alignTransform.enabled = true;
 			
 			// Work out the new movement position.
 			var tf = transform;
 			var movementX = tf.right * input.x;
 			var movementZ = tf.forward * input.y;
-			var movementVector = (movementX + movementZ) * movementSpeed;
+			var movementVector = (movementX + movementZ) * _movementSpeed;
 			// Add the new position to the current position.
 			_rb.MovePosition(tf.position + movementVector);
 		}
