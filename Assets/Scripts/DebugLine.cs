6c2e082f6cdc5422017d4cabb4b0e285bb365256
Justin <jgrigas@elam.co.uk>
2019-05-08 13:46:36 +0100

    Working on NPC rotations

---
+++ Assets/Scripts/DebugLine.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class DebugLine : MonoBehaviour
+{
+	[SerializeField] private float _length;
+	[SerializeField] private Color _lineColour;
+	private void Update()
+	{
+		var transform1 = transform;
+		var position = transform1.position;
+		var endPoint = position + transform1.forward * _length;
+		Debug.DrawLine(position, endPoint, _lineColour);
+	}
+}
--- Assets/Scripts/NpcRotations.cs
+++ Assets/Scripts/NpcRotations.cs
@@ -14,20 +14,25 @@ public class NpcRotations : MonoBehaviour
 
 	private void Update()
 	{
-		var yDifference = _camera.rotation.y - _parent.rotation.y;
+		var relative = Quaternion.Inverse(_camera.rotation) * _parent.rotation;
+		
+		var yDifference = _parent.rotation.y - _camera.rotation.y;
 		// Back.
-		if (yDifference > -.25f && yDifference < .25f)
+		if (yDifference > -.2f && yDifference < .2f)
 		{
 			_spriteRenderer.sprite = _back;
+			_spriteRenderer.flipX = false;
 		}
-		else if (yDifference > .75f || yDifference < -.75f)
+		// Front.
+		else if (yDifference > .9f || yDifference < -.9f)
 		{
 			_spriteRenderer.sprite = _forward;
+			_spriteRenderer.flipX = false;
 		}
+		// Sides.
 		else
 		{
 			_spriteRenderer.sprite = _side;
 		}
-		print(yDifference);
 	}
 }
