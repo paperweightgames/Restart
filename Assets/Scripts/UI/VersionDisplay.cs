f70fe3eb624cdf6f3521173a095042c359750290
Justin <jgrigas@elam.co.uk>
2019-05-16 11:56:55 +0100

    WIP

---
+++ Assets/Scripts/UI/VersionDisplay.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI {
+	[RequireComponent(typeof(Text))]
+	public class VersionDisplay : MonoBehaviour
+	{
+		private Text _displayText;
+		[SerializeField] private string _formatting;
+
+		private void Awake()
+		{
+			_displayText = GetComponent<Text>();
+		}
+
+		private void Start()
+		{
+			var version = Application.version;
+			_displayText.text = string.Format(_formatting, version);
+		}
+	}
+}
